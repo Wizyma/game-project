e96ed3dca6aa2cc11709f1ae4f30566c26855efb
Andre Gomes <andregomes@MacBook-Pro-de-Andre.local>
2018-03-27 10:43:33 +0200

    init commit

---
+++ client/webpack.config.js
@@ -0,0 +1,37 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, '/dist')
+const APP_DIR = path.resolve(__dirname, 'client/')
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        APP_DIR + '/index.js'
+    ],
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        rules: [
+            {
+              test: /.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: ['babel-loader', 'eslint-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+};
+            
