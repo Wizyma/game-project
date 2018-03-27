b3bef675aa78f3897252761a0ce2255f62e673b4
Andre Gomes <andregomes@MacBook-Pro-de-Andre.local>
2018-03-27 14:17:57 +0200

    feat(schema.js | index.js | query.js | api): Add graphql schema and query
    Graphql Schema to query Games from the igdb api
    
    BREAKING CHANGE: The graphql server is now set up

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 import { Server } from './app/server'
+require('dotenv').config()
 
-const server = new Server({
-    port: process.env.PORT,
-    endpoint: '/graphql',
-    subscriptions: '/subscriptions',
-    playground: '/playground',
+const server = new Server({ // eslint-disable-line no-unused-vars
+	port: process.env.PORT,
+	endpoint: '/graphql',
+	subscriptions: '/subscriptions',
+	playground: '/playground'
 })
